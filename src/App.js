6b55c8fffa39046c9a079ff599c1dee885ef7ef4
Oscar Diaz <oscarock02@gmail.com>
2020-11-24 22:28:14 -0500

    setting fronted app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import {Container,Row,Col} from 'react-bootstrap';
+import {Container,Row,Col,Navbar} from 'react-bootstrap';
 
 import MapContainer from './components/MapContainer'
 import CardsWeather from './components/CardsWeather'
@@ -9,6 +9,12 @@ class App extends Component{
   render(){
     return <div>
       <Container>
+        <>
+          <Navbar bg="light">
+            <Navbar.Brand>Paperos del Occidente S.A.</Navbar.Brand>
+          </Navbar>
+        </><br />
+        <h2 style={{textAlign:'center'}}>Condiciones meteorológicas de la finca</h2>
         <MapContainer />
         <Row>
           <Col xs={6} md={12} style={{marginTop: '400px'}}>
